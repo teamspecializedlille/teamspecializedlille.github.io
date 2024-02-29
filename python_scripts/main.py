4038214fe88a489fd7927b33c48849a6253814ae
Felix <felix.moulin@decathlon.com>
2024-02-29 17:31:00 +0100

    challenge

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,13 +1,6 @@
 import team_members
 
 import parse_results
-print("update disable")
-team = {}
-
-team = team_members.load_team_from_file(team)
-team_members.update_challenge(team)
-
-exit(0)
 team = {}
 results = {}
 
@@ -17,4 +10,5 @@ team = team_members.load_team_from_file(team)
 parser = parse_results.ParseResults(team, results)
 team = parser.generate_results()
 team_members.update_team_file(team)
+team_members.update_challenge(team)
 print("Update done")
