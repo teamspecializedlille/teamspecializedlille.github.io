2394dbb7ec6b6893931047fb3548421ec8971d0c
Lobwick <Lobwick@users.noreply.github.com>
2024-11-09 08:43:41 +0000

    Deploy preview for PR 6 🛫

--- pr-preview/pr-6/assets/js/scripts.js
+++ pr-preview/pr-6/assets/js/scripts.js
@@ -1273,48 +1273,7 @@ $(function () {
     -------------------------------  Preloader svg   -------------------------------
     ============================================================================= */
 
-    const svg = document.getElementById("svg");
-    const tl = gsap.timeline();
-    const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-    const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
-
-    tl.to(".loader-wrap-heading .load-text , .loader-wrap-heading .cont", {
-        delay: 1.5,
-        y: -100,
-        opacity: 0,
-    });
-    tl.to(svg, {
-        duration: 0.5,
-        attr: { d: curve },
-        ease: "power2.easeIn",
-    }).to(svg, {
-        duration: 0.5,
-        attr: { d: flat },
-        ease: "power2.easeOut",
-    });
-    tl.to(".loader-wrap", {
-        y: -1500,
-    });
-    tl.to(".loader-wrap", {
-        zIndex: -1,
-        display: "none",
-    });
-    tl.from(
-        "header",
-        {
-            y: 200,
-        },
-        "-=1.5"
-    );
-    tl.from(
-        "header .container",
-        {
-            y: 40,
-            opacity: 0,
-            delay: 0.3,
-        },
-        "-=1.5"
-    );
+    
 
 });
 
